6d69c437243f8cba007186c35a9b6323c2a6cb7f
Oriol Grau <ograup@gmail.com>
2025-10-31 16:52:29 +0100

    feat: add top padding to layout

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ export default App
 function Layout({ children }: PropsWithChildren) {
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="container mx-auto px-4 lg:px-8">{children}</div>
+      <div className="container mx-auto px-4 py-6 lg:px-8">{children}</div>
     </div>
   )
 }
