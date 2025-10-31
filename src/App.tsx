5ca81f15a1f03c687dab01a622c4b6e4fd3f2ea9
Oriol Grau <ograup@gmail.com>
2025-10-31 16:06:42 +0100

    feat: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { PropsWithChildren } from 'react'
+import { Home } from './Home'
 import './index.css'
 
+const queryClient = new QueryClient()
+
 function App() {
-  return <div>Hello world</div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Home />
+      </Layout>
+    </QueryClientProvider>
+  )
 }
 
 export default App
+
+function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="container mx-auto px-4 lg:px-8">{children}</div>
+    </div>
+  )
+}
