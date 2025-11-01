375ce634a4cf8e955be6d32c69633e2c30a73c16
Oriol Grau <ograup@gmail.com>
2025-11-01 10:58:03 +0100

    feat: header title links to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom'
+
 export function Header() {
   return (
     <div className="flex flex-col mb-4">
       <h1 className="text-[#4886ab] text-[16px] font-semibold mb-1 text-left">
-        Podcaster
+        <Link
+          to="/"
+          className="hover:underline"
+        >
+          Podcaster
+        </Link>
       </h1>
       <div className="w-full h-0.5 bg-gray-200" />
     </div>
