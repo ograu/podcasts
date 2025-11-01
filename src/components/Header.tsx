b0608e585881a1e84a3acb6647cae7227b195a2b
Oriol Grau <ograup@gmail.com>
2025-11-01 11:54:23 +0100

    feat: loading indicator

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom'
 
-export function Header() {
-  return (
-    <div className="flex flex-col mb-4">
+export const Header = ({ isLoading }: { isLoading: boolean }) => (
+  <div className="flex flex-col mb-4 relative">
+    <div className="flex justify-between">
       <h1 className="text-[#4886ab] text-[16px] font-semibold mb-1 text-left">
         <Link
           to="/"
@@ -11,7 +11,15 @@ export function Header() {
           Podcaster
         </Link>
       </h1>
-      <div className="w-full h-0.5 bg-gray-200" />
+      <div className="ml-2">
+        {isLoading ? (
+          <span
+            className="inline-block w-3 h-3 rounded-full bg-[#4886ab] animate-pulse"
+            aria-label="Loading"
+          />
+        ) : null}
+      </div>
     </div>
-  )
-}
+    <div className="w-full h-0.5 bg-gray-200" />
+  </div>
+)
